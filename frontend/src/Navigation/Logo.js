34d68275b5478efd299e234701ea77e14bbda114
Kostadin Devedzhiev <48786151+KostadinDev@users.noreply.github.com>
2022-03-29 03:52:36 -0400

    Delete frontend directory

--- frontend/src/Navigation/Logo.js
+++
@@ -1,8 +0,0 @@
-import logo from './logo-transparent-removebg-preview.png';
-import './navigation.style.css'
-
-export default function Logo(props) {
-    return <div className= 'logo-container' >
-        <h1 className="logo">Re<i className="logo-cursive">cursive</i> QA</h1>
-    </div>
-}
